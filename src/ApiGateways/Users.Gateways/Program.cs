120ae3daeb66cfc92c97a101f0b58fcc340e601f
iastalus <iualian@askia.com>
2023-03-20 14:45:00 +0200

    Implemented some gateways

--- src/ApiGateways/Users.Gateways/Program.cs
+++ src/ApiGateways/Users.Gateways/Program.cs
@@ -7,12 +7,15 @@ builder.Host.ConfigureLogging((ctx, loggingbuilder) =>
     loggingbuilder.AddConfiguration(ctx.Configuration.GetSection("Logging"));
     loggingbuilder.AddConsole();
     loggingbuilder.AddDebug();
+}).ConfigureAppConfiguration((ctx,cfg) =>
+{
+    cfg.AddJsonFile($"gatewayconfig.{ctx.HostingEnvironment.EnvironmentName}.json",true,true);
 });
 builder.Services.AddOcelot();
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
-app.UseOcelot();
+app.UseOcelot().Wait();
 
 app.Run();
